2859ce35fe3e96188f6b9eb075d556c9f2a6bdf8
Himanshu Nautiyal <47600885+Himjo@users.noreply.github.com>
2020-06-13 15:42:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ echo ' <marquee> WECOME TO MY CLOUD WORLD </marquee> ';
 echo' 
 <html>
 <center> <img src="https://github.com/Himjo/terra-image.git"  width="300" height="400">  </center>
-';
+
 </html>
+';
 echo'
 <center> <h2> Terraform </h2> </center>
      <p> Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </p>
