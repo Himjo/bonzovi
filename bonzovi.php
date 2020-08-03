cd9ecbadb8483e798576cce551164aa1c0725778
Himanshu Nautiyal <47600885+Himjo@users.noreply.github.com>
2020-08-03 14:08:32 +0530

    Create bonzovi.php

---
+++ bonzovi.php
@@ -0,0 +1,9 @@
+<marquee> WECOME TO ANSIBLE AUTOMATION WORLD - BY MR. VIMAL DAGA </marquee> 
+<br />
+<pre>
+<?php
+echo'
+<center> <h2> Ansible -  Simplest way to automate apps and IT infrastructure. Application Deployment + Configuration Management + Continuous Delivery </h2> </center>
+';
+?>
+</pre>
